a1359516b7d6da49502d7ade331883153eb06dd3
Jonathan Martins <jonhmbc@gmail.com>
2017-01-25 03:45:56 -0300

    Added ajax in delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,9 +17,12 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = @post.comments.find(params[:id])
-    if @comment.destroy
-      flash[:success] = "Comment deleted!"
-      redirect_to :back
+    if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     end
   end
 
