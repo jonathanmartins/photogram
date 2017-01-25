75ebefb337d53dbead3b7be9f356ae56cdbb2ec4
Jonathan Martins <jonhmbc@gmail.com>
2017-01-24 22:45:34 -0300

    Added set post method in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -8,27 +10,28 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path(@post)
+    else
+      render :edit
     end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -36,5 +39,9 @@ class PostsController < ApplicationController
   private
     def post_params
       params.require(:post).permit(:caption, :image)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 end
