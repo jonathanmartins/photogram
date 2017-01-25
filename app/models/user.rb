fb37de478402cd775fe230d9def27b3e774e6010
Jonathan Martins <jonhmbc@gmail.com>
2017-01-25 01:37:32 -0300

    Added username to user and link users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, length: { minimum: 4, maximum: 16 }
+
+  has_many :posts, dependent: :destroy
 end
