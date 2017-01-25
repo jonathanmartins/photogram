6585f8f7f569888db2b20192997c235a372a2fc3
Jonathan Martins <jonhmbc@gmail.com>
2017-01-25 03:15:42 -0300

    Finished comments features

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 4, maximum: 16 }
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
