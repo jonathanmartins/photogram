24b7c029ab211d000ebc7b684bdd82218e8c2671
Jonathan Martins <jonhmbc@gmail.com>
2017-02-09 19:25:14 -0300

    Added div showing likes on post #1

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def likers_of post
+    votes = post.votes_for.up.by_type User
+    usernames = []
+    unless votes.blank?
+      votes.voters.each do |voter|
+        usernames.push(link_to voter.username, profile_path(voter.username ), class: 'username')
+      end
+      usernames.to_sentence.html_safe + like_plural(votes)
+    end
+  end
+
+  private
+
+  def like_plural votes
+    return votes.count > 1 ? ' likes this' : ' like this'
+  end
 end
