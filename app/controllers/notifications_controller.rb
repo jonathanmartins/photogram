2a26e1abb3b9bc4f48f337a5243dcad70f0d4872
Jonathan Martins <jonhmbc@gmail.com>
2017-02-13 05:53:07 -0300

    Added index notifications to show them all #2

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,8 @@
 class NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.notifications
+  end
+
   def link_through
     @notification = Notification.find(params[:id])
     @notification.update read: true
