4c13bfb9ae68d37bbe0dd4fbbd352c8b081e5f50
Jonathan Martins <jonhmbc@gmail.com>
2017-02-13 21:46:19 -0300

    Added follow and unfollow buttons + ajax + js #3

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,27 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:follow_user, :unfollow_user]
+
   def follow_user
+    if current_user.follow @user.id
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
+    end
   end
 
   def unfollow_user
+    if current_user.unfollow @user.id
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(username: params[:username])
   end
 end
