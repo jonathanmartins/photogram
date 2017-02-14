a95c45d28410dd899ca11e9c1da0d1ad16fb8bf7
Jonathan Martins <jonhmbc@gmail.com>
2017-02-13 21:10:45 -0300

    Added relationships between users to follow and unfollow #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get ':username/follow_user', to: 'relationships#follow_user', as: :follow_user
+  get ':username/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
+
   get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
   get 'notifications', to: 'notifications#index'
 
